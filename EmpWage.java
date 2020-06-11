9ab9b6d6d356d234f7df61496cf47e983e242a35
SurajSingh01 <singhsurajd03@gmail.com>
2020-06-11 21:48:26 +0530

    Part time employee wage is calculated

--- EmpWage.java
+++ EmpWage.java
@@ -2,19 +2,22 @@
 public class EmpWage{
 	public static void main(String[] args){
 		//Constant
+		int IS_FULL_TIME=1;
+		int IS_PART_TIME=2;
 		int WAGE_PER_HOUR=20;
 		int FULL_DAY_HOURS=8;
 		//variable
-		double empCheck = Math.floor(Math.random() * 10 ) %2;
+		double empCheck = Math.floor(Math.random() * 10 ) %3;
 		int dailyWage=0;
 
 		System.out.println("Welcome Wage Employee..");
 		//Computation
-		if (empCheck == 1)
+		if (empCheck == IS_FULL_TIME)
 		{
-			System.out.println("Employee is Present.");
-			dailyWage= WAGE_PER_HOUR * FULL_DAY_HOURS;
+			dailyWage= WAGE_PER_HOUR * 8;
 		}
+		else if (empCheck == IS_PART_TIME)
+			dailyWage= WAGE_PER_HOUR *4;
 		else
 		{
 			System.out.println("Employee is Absent.");
