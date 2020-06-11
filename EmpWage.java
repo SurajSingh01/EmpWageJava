482571f96edd326f65dda8554bc378fca2780194
SurajSingh01 <singhsurajd03@gmail.com>
2020-06-11 21:58:16 +0530

    Solving with the use of switch case

--- EmpWage.java
+++ EmpWage.java
@@ -7,21 +7,24 @@ public class EmpWage{
 		int WAGE_PER_HOUR=20;
 		int FULL_DAY_HOURS=8;
 		//variable
-		double empCheck = Math.floor(Math.random() * 10 ) %3;
+		int empCheck = (int)Math.floor(Math.random() * 10 ) %3;
 		int dailyWage=0;
+		int empHrs=0;
 
 		System.out.println("Welcome Wage Employee..");
 		//Computation
-		if (empCheck == IS_FULL_TIME)
-		{
-			dailyWage= WAGE_PER_HOUR * 8;
-		}
-		else if (empCheck == IS_PART_TIME)
-			dailyWage= WAGE_PER_HOUR *4;
-		else
-		{
-			System.out.println("Employee is Absent.");
-		}
+		switch (empCheck){
+				case 0:
+							empHrs=0;
+							break;
+				case 1:
+							empHrs=4;
+							break;
+				case 3:
+							empHrs=8;
+							break;
+			}
+		dailyWage= WAGE_PER_HOUR * empHrs;
 		System.out.println("Employee Daily Wage=" +dailyWage);
 	}
 }
